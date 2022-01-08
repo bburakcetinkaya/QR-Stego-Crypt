c792254f408467d905a8bacb678aa50e6460a43c
BURAK <tr.burak.cetinkaya@gmail.com>
2022-01-08 05:59:49 +0300

    keygen update

--- key_gen.m
+++ key_gen.m
@@ -1,35 +1,85 @@
-function key = key_gen(input,s)
+function key = key_gen(time, password, s)
 
-epoch = seconds(datetime('now') - datetime(1970,1,1));
+epoch = time;
+epoch = milliseconds(datetime('now') - datetime(1970,1,1));
 
-epochbin = zeros(s(1),s(2));
-epochtemp = epoch;
-while epochtemp>=0
+s(1) = 512;
+s(2) = 512;
+iter1 = max(ceil(log10(abs(epoch))),1);
+
+epochTempArr = zeros(1,iter1);
+
+for i = 1:iter1
+    epochtemp = epoch/(10^(iter1-i));
+    epochtemp = floor(epochtemp);
+    epochTempArr(i) = mod(epochtemp,10); 
+    
+    for j=(i)*8:-1:((i-1)*8)+1
+        epochbin(j) = mod(epochTempArr(i),2);
+        epochTempArr(i) = epochTempArr(i)./2;
+        epochTempArr(i) = floor(epochTempArr(i));
+    end
+end
+epochbin = uint8(epochbin);
+
+epochKey = zeros(s(1),s(2));
 for i = 1:s(1)
     for j = 1:s(2)
-            epochbin(i,j) = epoch/2^(i+j-2);
-            epochtemp = epochtemp/10;
+        if (i+j) < size(epochbin,2)
+         epochKey(i,j) = epochbin(i+j); 
+
+        else
+            break;
+        end
     end
 end
+
+epochKey = circshift(epochKey,[111 360]);
+% epochKey = randperm(numel(epochKey));
+% epochKey = reshape(epochKey,s);
+epochKey = uint8(epochKey);
+
+asciiPass = double(char('lşkfdslşkdfkşlsfdkşlfds65+565+6+56a+sdfwe+a5905 +9we 0*-wa/9s+adf5 +6'));
+
+iter2 = size(asciiPass,2);
+asciiTemp = zeros(1,iter2*3);
+temp = asciiPass;
+
+for i = 1:iter2
+    for k = (i-1)*3+1:(i*3)
+        temp(k) = asciiPass(i)/(10^mod(3-k,3)); %max 3 digits in ascii charachers
+        temp(k) = floor(temp(k));
+        asciiTemp(k) = mod(temp(k),10); 
+    end  
 end
+for i= 1:(iter2*3)
+             for j=(i*8):-1:((i-1)*8)+1
+                passbin(j) = mod(asciiTemp(i),2);
+                asciiTemp(i) = asciiTemp(i)./2;
+                asciiTemp(i) = floor(asciiTemp(i));
+             end
+end
+passKey = zeros(s(1),s(2));
+for i = 1:s(1)
+    for j = 1:s(2)
+        if (i+j) < size(passbin,2)
+         passKey(i,j) = passbin(i+j); 
 
-
-inputmat = zeros(s(1),s(2));
-inputbin = reshape(dec2bin(input, 8).'-'0',1,[]);
-
-for i = 1:size(epochbin(1))
-    for j = 1:size(epochbin(2))
-       inputmat(i,j) = epochbin(i,j);
+        else
+            break;
+        end
     end
 end
-% for i = 1:inputbin.length()
-%     for j = 1:inputbin.width()
-%        inputmat(i,j) = inputbin(i,j);
-%     end
-% end
-%     
-%     
-% key = bitxor(epoch,input);
+
+passKey = circshift(passKey,[125 250]);
+% passKey = randperm(numel(passKey));
+% passKey = reshape(passKey,s);
+passKey= uint8(passKey);
+
+key = bitxor(passKey,epochKey);
+% key = randperm(numel(key));
+% key = reshape(key,s);
+key= uint8(key);
 
 
 
